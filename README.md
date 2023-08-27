3693b3dd032d8f481fb83a798701e2ebdd6cf590
Celso Aleixo celso.aleixo@uello.com.br <celso.aleixo@uello.com.br>
2023-08-27 13:17:06 -0300

    proj: criando um contador com livewire

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 #### Instalação Livewire
 * `composer require livewire/livewire`
+
+### Projeto ≥ Contador:
+* `sail art make:livewire contador`
+* 
---
+++ resources/views/livewire/contador.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h1>Meu contador</h1>
+    <button wire:click="decrement">-</button>
+    {{ $count }}
+    <button wire:click="increment">+</button>
+</div>
