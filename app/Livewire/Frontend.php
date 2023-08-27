eff2cdf028d4377a9c9c54a2e33469c30938b6e0
Celso Aleixo celso.aleixo@uello.com.br <celso.aleixo@uello.com.br>
2023-08-27 17:53:34 -0300

    proj2: form de contato parte 1 estrutura

--- app/Livewire/Frontend.php
+++ app/Livewire/Frontend.php
@@ -8,23 +8,6 @@ use Livewire\Component;
 
 class Frontend extends Component
 {
-    public int $defaultCounter = 0;
-
-    public function mount(int $id): void
-    {
-        $this->defaultCounter = $id;
-    }
-
-//    /**
-//     * @param App\Models\Customer $customer
-//     * @Annotation Exemplo de router model bind no livewire
-//     * @return void
-//     */
-//    public function mount(Customer $customer): void
-//    {
-//        // implements
-//    }
-
     public function render()
     {
         return view('livewire.frontend');
--- resources/views/livewire/frontend.blade.php
+++ resources/views/livewire/frontend.blade.php
@@ -1,9 +1,11 @@
 {{--Component Pai--}}
-<div class="bg-gray-300 container mx-auto w-1/2">
-    <h1>Meus Componentes</h1>
+<div class="p-6 bg-gray-200 container mx-auto w-1/2">
+    <h1 class="text-2xl text-center">Meus Componentes</h1>
 
     {{-- Componente filho (reuso) --}}
     {{-- Ao passar um paramentro (:count="$defaultCounter") para o componente filho, o mesmo se torna prioridade de inicialização do atributo --}}
     {{-- Prioridade maxima depois do mount --}}
-    <livewire:contador :count="$defaultCounter" />
+{{--    <livewire:contador :count="$defaultCounter" />--}}
+
+    <livewire:form />
 </div>
